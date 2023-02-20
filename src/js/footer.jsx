2875cfc539643b39ec824c272fb5a5f4c8770385
yangxdev <yangxdev@gmail.com>
2023-02-20 21:14:46 +0100

    hotfix: submit button

--- src/js/footer.jsx
+++ src/js/footer.jsx
@@ -6,6 +6,11 @@ class Footer extends Component {
     // console.log("Footer mounted, current step: " + this.props.step);
   }
 
+  componentDidUpdate() {
+    this.refreshButtons();
+    // console.log("Footer updated, current step: " + this.props.step);
+  }
+
   refreshButtons = () => {
     const btnBack = document.querySelectorAll(".btn-back");
     btnBack.forEach((btn) => {
@@ -55,13 +60,23 @@ class Footer extends Component {
         >
           Go Back
         </button>
-        <button
-          type="button"
-          className="btn btn-primary btn-lg btn-block btn-next"
-          id="btn-next"
-        >
-          Next Step
-        </button>
+        {this.props.step === 4 ? (
+          <button
+            type="button"
+            className="btn btn-primary btn-lg btn-block btn-submit"
+            id="btn-submit"
+          >
+            Submit
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-primary btn-lg btn-block btn-next"
+            id="btn-next"
+          >
+            Next Step
+          </button>
+        )}
       </div>
     );
   }
